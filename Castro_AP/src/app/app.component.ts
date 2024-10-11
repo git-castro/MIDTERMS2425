af58b9c066672c8cc0bc23a2ab49bb82b42011dd
git-castro <20193463@s.ubaguio.com>
2024-10-11 13:44:07 +0800

    -m 10/11/24

--- Castro_AP/src/app/app.component.ts
+++ Castro_AP/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    NavbarComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
