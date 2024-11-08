b83c15587cd292207a2e63750e84e63faabe0b51
git-castro <20193463@s.ubaguio.com>
2024-11-08 13:25:19 +0800

    -m 11/08/24 (Midterm Exam)

---
+++ Castro_ME/src/app/app.routes.ts
@@ -0,0 +1,33 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+
+export const routes: Routes = [
+    {
+        path: 'home', 
+        component: HomeComponent
+    },
+    {
+        path: 'about', 
+        component: AboutComponent
+    },
+    {
+        path: 'home/about', 
+        component: AboutComponent
+    },
+    {
+        path: 'services', 
+        component: ServicesComponent
+    },
+    {
+        path: 'contact', 
+        component: ContactComponent
+    },
+    {
+        path: '**', 
+        component: NotfoundComponent
+    }
+];
