118803bc47a0a95d44959f6776a342cf7ac342f8
git-castro <20193463@s.ubaguio.com>
2024-11-20 13:12:28 +0800

    -m 11/20/24 (Angular Forms Activity)

--- Castro_ME/src/app/contact/contact.component.ts
+++ Castro_ME/src/app/contact/contact.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  model = { name: '', email: '', phone: '', message: '' };
+  @ViewChild('modal') modal!: ElementRef;
+  openModal() {
+    const modalElement = this.modal.nativeElement;
+    modalElement.classList.add('show');
+    modalElement.style.display = 'block';
+    modalElement.removeAttribute('aria-hidden');
+    modalElement.setAttribute('aria-modal', 'true');
+  }
 
+  closeModal() {
+    const modalElement = this.modal.nativeElement;
+    modalElement.classList.remove('show');
+    modalElement.style.display = 'none';
+    modalElement.setAttribute('aria-hidden', 'true');
+    modalElement.removeAttribute('aria-modal');
+  }
 }
